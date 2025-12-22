93587317f200ff8008a192bb838ef37d28717fca
NickTheDevOpsGuy <limeman@gmail.com>
2025-12-22 17:12:00 -0500

    Adding in more changes

---
+++ src/services/faq/faqService.ts
@@ -0,0 +1,25 @@
+//src/services/faq/services.ts
+
+export function getAll() {
+
+}
+
+export function getByKey(key: string) {
+
+}
+
+export function create(entryInput) {
+
+}
+
+export function update(key: string, patch) {
+
+}
+
+export function remove(key: string) {
+
+}
+
+export function incrementUsage(key: string) {
+
+}
---
+++ src/services/faq/store.ts
@@ -0,0 +1,1 @@
+//src/services/faq/store.ts
