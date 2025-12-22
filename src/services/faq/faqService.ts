ba908db9771a1f1c0f6a2619ca1c7e3386ce7846
NickTheDevOpsGuy <limeman@gmail.com>
2025-12-22 17:12:05 -0500

    style: auto-format with Prettier [skip-precheck]

--- src/services/faq/faqService.ts
+++ src/services/faq/faqService.ts
@@ -1,25 +1,13 @@
 //src/services/faq/services.ts
 
-export function getAll() {
+export function getAll() {}
 
-}
+export function getByKey(key: string) {}
 
-export function getByKey(key: string) {
+export function create(entryInput) {}
 
-}
+export function update(key: string, patch) {}
 
-export function create(entryInput) {
+export function remove(key: string) {}
 
-}
-
-export function update(key: string, patch) {
-
-}
-
-export function remove(key: string) {
-
-}
-
-export function incrementUsage(key: string) {
-
-}
+export function incrementUsage(key: string) {}
